e806cba5d9f4ee4f09a13f6521f085d5a80a0052
Dan Lentz <danlentz@gmail.com>
2014-12-17 21:31:59 -0500

    new: node-filter

--- src/wbtree/tree.clj
+++ src/wbtree/tree.clj
@@ -542,6 +542,12 @@
     n n))
 
 
+(defn node-invert [n]
+  (node-fold-left (fn [acc x]
+                    (node-add acc (-v x) (-k x)))
+    (null) n))
+
+
 (defn node-iter
   "For the side-effect, apply f to each node of the tree rooted at n"
   [n f]
@@ -825,15 +831,21 @@
 ;; (def i30 (make-integer-tree 20 30))
 ;; (def i   (make-integer-tree 30))
 
-;; (map first (node-seq i))
+;; (map -k (node-seq i))
 ;;   => (0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
 ;;       25 26 27 28 29)
 
-;; (map first (node-seq-reverse i))
+;; (map -k (node-seq-reverse i))
 ;;   => (29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8
 ;;       7 6 5 4 3 2 1 0)
 
 
 
-;; (map first (node-seq (node-filter (comp even? -k) i)))
+;; (map -k (node-seq (node-filter (comp even? -k) i)))
 ;;   => (0 2 4 6 8 10 12 14 16 18 20 22 24 26 28)
+
+;; (def m (reduce #(apply node-add %1 %2) nil
+;;          [[1 :a][2 :b][3 :c][4 :d][5 :e]]))
+
+;; (map -k (node-seq (node-invert m)))
+;;   => (:a :b :c :d :e)
