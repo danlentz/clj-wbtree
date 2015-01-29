18bf77d292fb91b7eea6543403cc9cd27eaf9bb3
Dan Lentz <danlentz@gmail.com>
2015-01-29 10:03:01 -0500

    updated: remove clj-tuple dependancy; bump version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject danlentz/clj-wbtree "0.1.2-SNAPSHOT"
+(defproject danlentz/clj-wbtree "0.1.3-SNAPSHOT"
   :description "A 'Purely Functional' Weight Balanced Binary Tree"
-  :url "http://github.com/danlentz/clj-fgl"
+  :url "http://github.com/danlentz/clj-wbtree"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure  "1.6.0"]
-                 [clj-tuple            "0.1.7"]])
+  :dependencies [[org.clojure/clojure  "1.6.0"]])
+
